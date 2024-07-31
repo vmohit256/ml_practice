6f06b35b868c45db5a25ad40fcac702ad95be718
vmohit256 <153658146+vmohit256@users.noreply.github.com>
2024-07-31 15:19:46 +0530

    implement lasso

--- classical_ml/linear_regression/hg_linear_regression.py
+++ classical_ml/linear_regression/hg_linear_regression.py
@@ -20,7 +20,9 @@ class LinearRegression:
         """
         self.N, self.d = X.shape
         X = np.hstack([np.ones((self.N, 1)), X])
-        shifted_gram_matrix = X.T @ X + self.alpha * np.eye(self.d+1)
+        penalty = self.alpha * np.eye(self.d+1)
+        penalty[0, 0] = 0  # remove penalty on bias term
+        shifted_gram_matrix = X.T @ X + penalty
         determinant = np.linalg.det(shifted_gram_matrix)
         
         """
@@ -32,9 +34,73 @@ class LinearRegression:
         self.coef_ = self.W[1:]
         self.intercept_ = self.W[0]
 
+    def _fit_gd(self, X, y, lr=0.01, num_iters=1000):
+        """
+        For L2 penalty:
+        Loss(w) = ||X w - y||^2 + alpha * ||w||^2
+        del(Loss) / del(w) = 2 X^T X w - 2 X^T y + 2 alpha w
+        Gradient descent step:
+        w = w - lr * del(Loss) / del(w)
+
+        For L1 penalty:
+        Loss(w) = ||X w - y||^2 / (2 * num_samples) + alpha * ||w||_1
+        subgradient, del(Loss) / del(w) = (X^T X w - X^T y)  / (2 * num_samples) + alpha * sign(w)  // sign(x) = 1 if x > 0, -1 if x < 0, 0 if x = 0
+        """
+
+        assert self.penalty_norm in ['l2', 'l1'], f'Invalid penalty_norm: {self.penalty_norm}. Must be in ["l2", "l1"]'
+        self.N, self.d = X.shape
+        self.W = np.zeros((self.d+1, 1))
+        X = np.hstack([np.ones((self.N, 1)), X])
+
+        xtx = 2 * X.T @ X
+        xty = 2 * X.T @ y
+        xty = xty.reshape(-1, 1)
+
+        def get_gradient(W):
+            gradient = xtx @ W - xty
+            if self.penalty_norm == 'l2':
+                # ridge prevents sparsity because gradient of l2 norm is small when w is close to 0. So it doesn't push w to 0
+                penalty_gradient = self.alpha * W
+                penalty_gradient[0] = 0  # remove penalty on bias term
+            elif self.penalty_norm == 'l1':
+                # lasso promotes sparsity becasue gradient of l1 norm is big when w is close to 0
+                penalty_gradient = self.alpha * np.sign(W)
+                penalty_gradient[0] = 0
+                gradient = gradient / (2 * self.N)
+            return gradient + penalty_gradient
+        
+        def get_loss(W):
+            loss = np.linalg.norm(X @ W - y) ** 2 
+            if self.penalty_norm == 'l2':
+                norm = np.linalg.norm(W[1:]) ** 2
+                penalty = self.alpha * norm
+                loss += penalty
+            elif self.penalty_norm == 'l1':
+                norm = np.linalg.norm(W[1:], ord=1)
+                penalty = self.alpha * norm
+                loss = loss / (2 * self.N) + penalty
+            return loss
+
+        losses = []
+        for iteration in range(num_iters):
+            gradient = get_gradient(self.W)
+            self.W -= lr * gradient
+            if iteration % 100 == 0:
+                loss = get_loss(self.W)
+                losses.append(loss)
+                print(f'Iteration: {iteration}, Loss: {loss}')
+            if np.linalg.norm(gradient) < 1e-6:
+                break
+        self.coef_ = self.W[1:, 0]
+        self.intercept_ = self.W[0, 0]
+        return losses
+
+
     def fit(self, X, y):
         if self.solver == 'closed_form':
-            self._fit_closed_form(X, y)
+            return self._fit_closed_form(X, y)
+        elif self.solver == 'gd':
+            return self._fit_gd(X, y)
         else:
             raise NotImplementedError(f'Solver {self.solver} not implemented.')
 
